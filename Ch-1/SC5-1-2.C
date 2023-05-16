b1048a2e9bd7320362ac9bf581b1d2168882f8de
IamSahilvaghela <sahilvaghel7@gmail.com>
2023-05-16 19:17:01 +0530

    SV | 16/05/2023 | ch-1

---
+++ Ch-1/SC5-1-2.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	clrscr();
+	printf("Name\t\t: Vaghela Sahil\n");
+	printf("Birth Date\t: 26\\02\\2006\n");
+	printf("Age\t\t: 17\n");
+	printf("Education\t: Diploma in Computer engg.\n");
+	printf("Univercity\t: Uka Tarsadia Univercity\n");
+	printf("Course\t\t: C Language\n");
+	printf("Hobby\t\t: Coding\n");
+	printf("Fav. Colour\t: White ");
+	getch();
+
+}
---
+++ Ch-1/SC5-1-2_.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	clrscr();
+	printf("--------\n");
+	printf("|      |\n");
+	printf("S      |\n");
+	printf("A      |\n");
+	printf("H      |\n");
+	printf("I      |\n");
+	printf("L      |\n");
+	printf("|      |\n");
+	printf("--------\n");
+	getch();
+
+}
