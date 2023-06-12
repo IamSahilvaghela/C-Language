691368834972a3a09ca4a202b9270aa2354cf2b5
IamSahilvaghela <sahilvaghel7@gmail.com>
2023-06-12 18:47:23 +0530

    SV | 12/06/2023 | PR-4 : circlet

---
+++ PR-4/SC-PR4-5.C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int i,j,s,space=4;
+	clrscr();
+
+	for(i=1; i<=5; i++)
+	{
+		for(j=1; j<=i; j++)
+		{
+			printf("%d",j);
+		}
+		for(s=1; s<=space; s++)
+		{
+			printf("  ");
+		}
+		space--;
+		for(j=i; j>=1; j--)
+		{
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+	getch();
+}
